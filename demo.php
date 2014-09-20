d39595e10bcde7ef0ce7dd9391dc01733afc145e
Richard Hancock <me@richardhancock.co.uk>
2014-09-20 18:23:46 +0100

    Basic Testing Passed (Functional)

--- demo.php
+++ demo.php
@@ -8,9 +8,134 @@
 <body>
 	<h1>Contact Form Demo Page</h1>
 
-	<form action="validation.php" method="post">
+	<?php
+		if (isset($_POST["Submitted"]))
+		{
+			//Functions
+			function hasContent($input)
+			{
+				if (strlen($input) <= 0) {
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+
+			//Initialize Error bool array
+			$errors = array(
+				'name' => false,
+				'email' => false,
+				'subject' => false,
+				'message' => false,
+				'captcha' => false,
+				 );
+
+			//Initialize error message array
+			$errorStrings = array(
+				'name' => "Please insert your name", 
+				'email' => "Please insert a valid email address",
+				'subject' => "Please insert a subject",
+				'message' => "Please insert a message",
+				'captcha' => "", //Gets generated later by the captcha php
+			);
+
+
+			//Recaptcha required code:
+			require_once('recaptchalib.php');
+			$privatekey = ""; //CHANGE THIS to your own key
+			$resp = recaptcha_check_answer ($privatekey,
+			                                $_SERVER["REMOTE_ADDR"],
+			                                $_POST["recaptcha_challenge_field"],
+			                                $_POST["recaptcha_response_field"]);
+
+			if (!$resp->is_valid) {
+			    // What happens when the CAPTCHA was entered incorrectly
+			    //die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+			         //"(reCAPTCHA said: " . $resp->error . ")");
+				$errors['captcha'] = true;
+				if ($resp->error == "incorrect-captcha-sol") 
+				{
+					$errorStrings['captcha'] = "Incorrect captcha was entered";
+				} else {
+					$errorStrings['captcha'] = "An unknown error within captcha has occurred";
+				}
+			}
+
+			//Fetch Value from the forms POST data
+			$name = $_POST["Name"];
+			$email = $_POST["Email"];
+			$subject = $_POST["Subject"];
+			$message = $_POST["Message"];
+
+			$mailTo = ""; // CHANGE THIS to your own email address
+
+			//Error checking section
+			//Section might be able to be improved like this: $errors['name'] = !hasContent($name);
+			if (hasContent($name) == false)
+			{
+				$errors['name'] = true;
+			}
+
+			// This should check most emails but might not accept rarer email formats.
+			if (hasContent($email) == false || !preg_match('/^([A-Z0-9\.\-_]+)@([A-Z0-9\.\-_]+)?([\.]{1})([A-Z]{2,6})$/i',$email)) 
+			{
+				$errors['email'] = true;
+			}
+
+			if (hasContent($subject) == false) 
+			{
+				$errors['subject'] = true;
+			}
+
+			if (hasContent($message) == false)
+			{
+				$errors['message'] = true;
+			}
+
+
+			$errorCount = 0;
+
+			foreach ($errors as $key => $value) {
+				
+				if ($value == true) {
+					$errorCount++;
+				}
+			
+			}
+
+			// Final check and Email Sender
+			if ($errorCount == 0) {
+				
+				//Email Construction
+				$headers  = 'MIME-Version: 1.0' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$headers .= 'From: '.$email."\r\n";
+				$headers .= 'Reply-To: '.$email."\r\n";
+
+				$body = "<html><body><b>Name:</b>".$name."<br>";
+				$body .= "<b>Email:</b>".$email."<br>";
+				$body .= "<b>Message:</b>"."<br>";
+				$body .= $message."<br></body></html>";
+
+				//CF is a tag you can set you mail program to auto allow
+				$finalSubject = "CF: ".$subject;
+
+				$result = 0;
+				if (mail($mailTo, $finalSubject, $body, $headers)) {
+					$result = 1;
+				} else {
+					$result = 2;
+				}
+
+			}
+		}
+	?>
+
+	<form action="demo.php" method="post">
 		<label>
-			Name: <input type="text" name="Name" 
+			Name: <input required type="text" name="Name" 
 			<?php 
 				if($errors['name']) {
 					echo('class="error" placeholder="'.$errorStrings["name"].'">');
@@ -21,7 +146,7 @@
 		</label><br>
 
 		<label>
-			Email: <input type="email" name="Email" 
+			Email: <input required type="email" name="Email" 
 			<?php 
 				if($errors['email']) {
 					echo('class="error" placeholder="'.$errorStrings["email"].'">');
@@ -32,7 +157,7 @@
 		</label><br>
 
 		<label>
-			Subject: <input type="text" name="Subject" 
+			Subject: <input required type="text" name="Subject" 
 			<?php 
 				if($errors['subject']) {
 					echo('class="error" placeholder="'.$errorStrings["subject"].'">');
@@ -46,149 +171,35 @@
 			Message: <textarea required name="Message" 
 			<?php 
 				if($errors['message']) {
-					echo('class="error" placeholder="'.$errorStrings["message"].'">');
+					echo('class="error" placeholder="'.$errorStrings["message"].'"></textarea>');
 				} else {
-					echo('class="formInput" placeholder="Message">');
+					echo('class="formInput" placeholder="Message"></textarea>');
 				}
 			?>
-			</textarea>
+			
 		</label><br>
 
 		<?php
 			require_once('recaptchalib.php');
- 			$publickey = "your_public_key";
+ 			$publickey = ""; //CHANGE THIS to your own key
   			echo recaptcha_get_html($publickey);
   		?>
-  		<br>
-		<input type="submit" name="Submit" value="Submit">
+
+  		<input type="hidden" name="Submitted" value=1>
+		<input type="submit" name="Submit" value="Submit"><br>
 		<span class="formResult">
 			<?php 
 				if($errors['captcha']) {
 					echo($errorStrings['captcha']);
 				} elseif ($errorCount != 0) {
 					echo("Please fix any mistakes and resubmit");
-				} elseif (!$result) {
+				} elseif ($result == 2) {
 					echo("Internal Server Error: Email did not send");
-				} elseif ($result) {
+				} elseif ($result == 1) {
 					echo("Email was sent successfully!");
 				}
 			?>
 		</span> 
 	</form>
-
-	<?php
-	if (isset($_POST["submit"]))
-	{
-		//Functions
-		function hasContent($input)
-		{
-			if ($input.len <= 0) {
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		}
-
-		//Initialize Error bool array
-		$errors = array(
-			'name' => false,
-			'email' => false,
-			'subject' => false,
-			'message' => false,
-			'captcha' => false,
-			 );
-
-		//Initialize error message array
-		$errorStrings = array(
-			'name' => "Please insert your name", 
-			'email' => "Please insert a valid email address",
-			'subject' => "Please insert a subject",
-			'message' => "Please insert a message",
-			'captcha' => "", //Gets generated later by the captcha php
-		);
-
-
-		//Recaptcha required code:
-		require_once('recaptchalib.php');
-		$privatekey = "your_private_key"; //CHANGE THIS to your own key
-		$resp = recaptcha_check_answer ($privatekey,
-		                                $_SERVER["REMOTE_ADDR"],
-		                                $_POST["recaptcha_challenge_field"],
-		                                $_POST["recaptcha_response_field"]);
-
-		if (!$resp->is_valid) {
-		    // What happens when the CAPTCHA was entered incorrectly
-		    //die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
-		         //"(reCAPTCHA said: " . $resp->error . ")");
-			$errors['captcha'] = $resp->error;
-		}
-
-		//Fetch Value from the forms POST data
-		$name = $_POST["Name"];
-		$email = $_POST["Email"];
-		$subject = $_POST["Subject"];
-		$message = $_POST["Message"];
-
-		$mailTo = "EMAILADDRESS"; // CHANGE THIS to your own email address
-
-		
-		//Error checking section
-		//Section might be able to be improved like this: $errors['name'] = !hasContent($name);
-		if (!hasContent($name))
-		{
-			$errors['name'] = true;
-		}
-
-		// This should check most emails but might not accept rarer email formats.
-		if (!hasContent($email) || !preg_match('/^([A-Z0-9\.\-_]+)@([A-Z0-9\.\-_]+)?([\.]{1})([A-Z]{2,6})$/i',$email)) 
-		{
-			$errors['email'] = true;
-		}
-
-		if (!hasContent($subject)) 
-		{
-			$errors['subject'] = true;
-		}
-
-		if (!hasContent($message))
-		{
-			$errors['message'] = true;
-		}
-
-
-		$errorCount = 0;
-
-		foreach ($errors as $key => $value) {
-			
-			if ($value) {
-				$errorCount++;
-			}
-
-		}
-
-		// Final check and Email Sender
-		if ($errorCount == 0 && strtoupper($mailTo) != "EMAILADDRESS") {
-			
-			//Email Construction
-			$headers  = 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-			$headers .= 'From: '.$email."\r\n";
-			$headers .= 'Reply-To: '.$email."\r\n";
-
-			$body = "<html><body><b>Name:</b>".$name."<br>";
-			$body .= "<b>Email:</b>".$email."<br>";
-			$body .= "<b>Message:</b>"."<br>";
-			$body .= $message."<br></body></html>";
-
-			//CF is a tag you can set you mail program to auto allow
-			$finalSubject = "CF: ".$subject;
-
-			$result = mail($mailTo, $finalSubject, $body, $headers);
-
-		}
-	}
-?>
 </body>
 </html>
